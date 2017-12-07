609f1ad4e59ee4c823f513b42ab847678f93aeae
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-12-07 09:45:54 +0100

    day7 part 1

--- 2017/day7/part1.js
+++ 2017/day7/part1.js
@@ -2,6 +2,33 @@ module.exports = input => result(input);
 
 function result(s) {
   let count = 0,
+    line;
 
-  return count;
+  // clean data
+  for (let i in s) {
+    line = s[i].split('->')
+    s[i] = {
+      id: line[0].split(' ')[0],
+      weight: line[0].split(' ')[1].slice(1, -1),
+      childlist: line[1] ? line[1].slice(1).split(', ') : []
+    }
+  }
+
+  return getParents(s).god;
+}
+
+
+function getParents(list) {
+  let parentid,
+    god;
+
+  for (let i in list) {
+    parentid = list.filter(v => v.childlist.indexOf(list[i].id) !== -1)[0]
+    parentid ? list[i].parentid = parentid.id : god = list[i].id
+  }
+  
+  return {
+    list: list,
+    god: god
+  };
 }
